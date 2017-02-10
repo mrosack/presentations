a52e83d9a545a52c7c31759c41d114056aac1e65
Mike Rosack <mike.rosack@omniresources.com>
2017-02-10 11:01:53 -0600

    basic aws demo

--- serverless-computing/web/src/app/aws/aws.component.ts
+++ serverless-computing/web/src/app/aws/aws.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-aws',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aws.component.css']
 })
 export class AwsComponent implements OnInit {
+  private responseJson: any;
+  private loading = false;
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
 
+  loadAws() {
+    this.load('https://7bsesjw2af.execute-api.us-east-1.amazonaws.com/dev/hello');
+  }
+
+  loadLocal() {
+    this.load('http://localhost:3000/hello');
+  }
+
+  load(url: string) {
+    delete this.responseJson;
+    this.loading = true;
+
+    this.http.get(url).forEach(resp => {
+      this.loading = false;
+      this.responseJson = resp.json();
+    });
+  }
 }
