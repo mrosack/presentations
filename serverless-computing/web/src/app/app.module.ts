702139f186ab36ea439f4c3164bf40449dfb2110
Mike Rosack <mike.rosack@omniresources.com>
2017-02-10 09:02:11 -0600

    add some components

--- serverless-computing/web/src/app/app.module.ts
+++ serverless-computing/web/src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AwsComponent } from './aws/aws.component';
+import { AzureComponent } from './azure/azure.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AwsComponent,
+    AzureComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'aws', pathMatch: 'full' },
+      { path: 'aws', component: AwsComponent },
+      { path: 'azure', component: AzureComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ serverless-computing/web/src/app/azure/azure.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { AzureComponent } from './azure.component';
+
+describe('AzureComponent', () => {
+  let component: AzureComponent;
+  let fixture: ComponentFixture<AzureComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AzureComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AzureComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
